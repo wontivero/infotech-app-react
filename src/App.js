7b366fd42e11eef979773c1737f777e8aa7311b9
wontivero <55588043+wontivero@users.noreply.github.com>
2022-04-18 20:06:49 -0500

    Segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-  return (
+  return (    
     <div className="App">
+      
+      <Navbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          E-commerce
+        <p>E-commerce
+          <h1 className="text-4xl font-bold underline">INFO-TECH</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Info-Tech
-        </a>
+      
       </header>
     </div>
   );
